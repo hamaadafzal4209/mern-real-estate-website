e7e521aec9d58699dbe92f047c4fa148856ab676
FrontendFlair <hamaadafzal404@gmail.com>
2024-05-01 05:05:42 -0700

    complete edit lising page functionallity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from "../src/pages/SignUp"
 import Header from "../src/components/Header"
 import PrivateRoute from "./components/PrivateRoute"
 import CreateListing from "./pages/CreateListing"
+import UpdateListing from "./pages/UpdateListing"
 
 function App() {
   return (
@@ -21,9 +22,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route element={<PrivateRoute />} >
             <Route path="/profile" element={<Profile />} />
-          </Route>
-          <Route element={<PrivateRoute />} >
             <Route path="/create-listing" element={<CreateListing />} />
+            <Route path="/update-listing/:listingId" element={<UpdateListing />} />
           </Route>
         </Routes>
       </BrowserRouter>

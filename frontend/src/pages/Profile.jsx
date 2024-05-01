5f1a3cdd38167d464b93821f029e321abb929bd4
FrontendFlair <hamaadafzal404@gmail.com>
2024-05-01 04:13:08 -0700

    complete delete user listing functionallity

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -151,6 +151,26 @@ function Profile() {
     }
   }
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/backend/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListing((prev) => prev.filter((listing) => listing._id !== listingId));
+
+    } catch (error) {
+      console.log(error.message)
+    }
+
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -200,7 +220,7 @@ function Profile() {
                 <p>{listing.name}</p>
               </Link>
               <div className="flex flex-col font-semibold items-center">
-                <button className='text-red-700 uppercase'>Delete</button>
+                <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
                 <button className='text-green-700 uppercase'>Edit</button>
               </div>
             </div>

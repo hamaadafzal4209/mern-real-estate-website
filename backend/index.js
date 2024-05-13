8c7df52208fc54db7327496565dbf2e8ad25460d
FrontendFlair <hamaadafzal404@gmail.com>
2024-05-13 06:38:39 -0700

    Remove Errors For Deployment

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use('/backend/listing', listingRouter);
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index,html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
